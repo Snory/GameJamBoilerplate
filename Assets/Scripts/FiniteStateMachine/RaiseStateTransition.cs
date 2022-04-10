b64f0436ac4d948b0882b0ba7c9061b3fa8c1238
Snory <robtill.in@gmail.com>
2022-04-10 17:50:34 +0200

    finishing examples for FSM

--- Assets/Scripts/FiniteStateMachine/RaiseStateTransition.cs
+++ Assets/Scripts/FiniteStateMachine/RaiseStateTransition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Tester : MonoBehaviour
+public class RaiseStateTransition : MonoBehaviour
 {
     public UnityEvent<ExampleStateEventArgument> TestEvent;
 
