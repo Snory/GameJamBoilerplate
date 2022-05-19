010fb47224399dd8935385644c527ea540149fb2
Snory <robtill.in@gmail.com>
2022-05-19 06:59:46 +0200

    generic repository

---
+++ Assets/Scripts/HighScoreData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class HighScoreData
+{
+    public string PlayerName;
+    public int Score;
+
+    public HighScoreData(string playerName, int score)
+    {
+        PlayerName = playerName;
+        Score = score;
+    }
+}
---
+++ Assets/Scripts/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IRepository<T>
+{
+    T Add(T item);
+    void Save();
+    IEnumerable<T> Load();
+    IEnumerable<T> Find(Func<T, bool> predicate);
+}
