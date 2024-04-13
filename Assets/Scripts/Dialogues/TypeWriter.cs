0e4cc920cf09eac941808a85472a10483615af66
Robin Till <robtill.in@gmail.com>
2024-04-13 07:16:49 +0200

    Remove state machine

--- Assets/Scripts/Dialogues/TypeWriter.cs
+++ Assets/Scripts/Dialogues/TypeWriter.cs
@@ -1,30 +1,39 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TypeWriter : MonoBehaviour
 {
     [SerializeField]
-    private float _speedInSec;
-   
+    private float _minSpeedInSec;
+    [SerializeField]
+    private float _maxSpeedInSec;
+
     public void Type(string text, Action<string> targetCallBack)
     {
-        StartCoroutine(TypeText(text,targetCallBack));
+        StartCoroutine(TypeText(text, targetCallBack));
     }
 
     private IEnumerator TypeText(string text, Action<string> targetCallback)
     {
         string typedText = "";
 
-        foreach(char letter in text.ToCharArray())
+        float speed = CalculateSpeed(text.Length);
+
+        foreach (char letter in text.ToCharArray())
         {
             typedText += letter;
             targetCallback(typedText);
 
-            yield return new WaitForSeconds(_speedInSec);
+            yield return new WaitForSeconds(speed);
         }
 
         yield return null;
+    }
+
+    private float CalculateSpeed(int textLength)
+    {
+        float speed = Mathf.Lerp(_maxSpeedInSec, _minSpeedInSec, textLength / 100f);
+        return speed;
     }
 }
